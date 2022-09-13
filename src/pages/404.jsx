006f97eee9d161a7de24fbe9821f3f3287856bc3
rachanont <lastelixer@gmail.com>
2022-09-13 16:57:18 +0700

    edit 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,6 +5,7 @@ const NotFound = () => (
   <Layout>
     <h1>404 - Page Not Found</h1>
     <p>Unfortunately we couldn't find what you were looking for :(</p>
+    <p><a href="https://www.rachanont.com/Pharmacist-salary">เงินเดือนเภสัชกรจบใหม่ ทำงานรพ.รัฐบาล (2565)</a></p>
   </Layout>
 )
 
